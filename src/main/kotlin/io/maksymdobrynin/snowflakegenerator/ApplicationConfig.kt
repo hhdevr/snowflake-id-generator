32e85a609bd708131c715a554e64b1e22eb49bd5
Dmitry Chaykin <hhdevr@gmail.com>
2025-11-30 21:05:53 +0900

    Homework 5: invocation of k8s variables add logging add endpoint to get k8s variables

--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/ApplicationConfig.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/ApplicationConfig.kt
@@ -1,12 +1,39 @@
 package io.maksymdobrynin.snowflakegenerator
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableConfigurationProperties(GeneratorSettingsProperties::class)
 open class ApplicationConfig {
-	@Bean
-	@ConfigurationProperties(prefix = "snowflake-settings")
-	open fun nexoDeviceHandlerSenderProperties(): GeneratorSettings = GeneratorSettings()
+
+    @Bean
+    open fun generator(settingsProps: GeneratorSettingsProperties): Generator {
+        val settings = GeneratorSettings(
+            startingEpoch = settingsProps.startingEpoch,
+            datacenterName = settingsProps.nodeName,
+            datacenterId = extract5Bits(settingsProps.nodeName),
+            datacenterIP = settingsProps.nodeIp,
+            workedName = settingsProps.podName,
+            workedId = extract5Bits(settingsProps.podName),
+            workedIP = settingsProps.podIp,
+            sequence = settingsProps.sequence
+        )
+        return Generator(settings)
+    }
+
+    private fun extract5Bits(name: String): Long =
+        name.hashCode().toLong() and 0x1F
 }
+
+@ConfigurationProperties(prefix = "snowflake-settings")
+data class GeneratorSettingsProperties(
+    var startingEpoch: Long = 946684800L,
+    var nodeName: String = "nodeName",
+    var nodeIp: String = "nodeIp",
+    var podName: String = "podName",
+    var podIp: String = "podIp",
+    var sequence: Long = 0L,
+)
--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/GeneratorSettings.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/GeneratorSettings.kt
@@ -3,8 +3,12 @@ package io.maksymdobrynin.snowflakegenerator
 data class GeneratorSettings(
 	// 00:00:00 on January 1st, 2000 (UTC)
 	var startingEpoch: Long = 946684800L,
+	var datacenterName: String = "datacenterName",
 	var datacenterId: Long = 1,
+	var datacenterIP: String = "datacenterIP",
+	var workedName: String = "workedName",
 	var workedId: Long = 1,
+	var workedIP: String = "workedIP",
 	var nextTimeSeed: () -> Long = { System.currentTimeMillis() },
 	var sequence: Long = 0L,
 )
