f90d3498da4881eeb0266d23bd45c29397d68f04
Dmitry Chaykin <hhdevr@gmail.com>
2025-12-23 20:37:09 +0600

    Homework 10: StatefulSet & DB connectivity

---
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/IdMetadata.kt
@@ -0,0 +1,35 @@
+package io.maksymdobrynin.snowflakegenerator
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "id_metadata")
+data class IdMetadata(
+
+    @Id
+    val guid: Long,
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime,
+
+    @Column(name = "datacenter_id")
+    val datacenterId: Long,
+
+    @Column(name = "worker_id")
+    val workerId: Long
+
+) {
+    constructor() : this(0L, LocalDateTime.now(), 0L, 0L)
+
+    override fun toString(): String {
+        return "IdMetadata(guid=$guid, " +
+            "createdAt=$createdAt, " +
+            "datacenterId=$datacenterId, " +
+            "workerId=$workerId)"
+    }
+
+}
--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
@@ -2,22 +2,34 @@ package io.maksymdobrynin.snowflakegenerator
 
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory.getLogger
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1")
-class SnowflakeController(private val generator: Generator) {
+class SnowflakeController(
+    private val generator: Generator,
+    private val service: IdMetadataService
+) {
 
     private val logger = getLogger(SnowflakeController::class.java)
 
     @GetMapping("/next-id")
     fun generate(): Long = runBlocking {
         val id = generator.nextId()
+        service.logIdMetadata(id, generator.getKubeSettings())
         logger.info("\n" + generator.getKubeDescription())
         logger.info("RESULT ID: $id")
         id
+    }
+
+    @GetMapping("/meta/{id}")
+    fun getMetadata(@PathVariable id: Long): ResponseEntity<String> {
+        val body = 	service.getLoggedIdMetadata(id).toString()
+        return ResponseEntity.ok(body)
     }
 
     @GetMapping("/ping")
